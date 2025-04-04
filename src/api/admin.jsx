c6f68dec8666fd85e70fd82f3105b1659804cbd4
Akbhartiq <adityakrbharti2005@gmail.com>
2025-04-05 00:58:06 +0530

    Driver@admin Done

--- src/api/admin.jsx
+++ src/api/admin.jsx
@@ -24,3 +24,23 @@ export const updateHotel = async (id, hotelData) => {
 export const deleteHotel = async (id) => {
     return await API.delete(`/admin/hotels/${id}`);
 };
+
+export const getDashboardData = async () => {
+    return await API.get('/admin/dashboard-data');
+}
+
+export const getDrivers = async () => {
+    return await API.get('/admin/drivers');
+}
+
+export const addDriver = async (driverData) => {    
+    return await API.post('/admin/drivers', driverData);
+}
+
+export const updateDriver = async (id, driverData) => {
+    return await API.put(`/admin/drivers/${id}`, driverData);
+}
+
+export const deleteDriver = async (id) => {
+    return await API.delete(`/admin/drivers/${id}`);
+}

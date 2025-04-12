ba45cc2fb825ad9b37847a9526dda945a8253abf
ipash185 <127605678+ipash185@users.noreply.github.com>
2025-04-12 21:58:58 +0530

    temp customer

--- src/api/customer.jsx
+++ src/api/customer.jsx
@@ -5,6 +5,38 @@ const API = axios.create({
     withCredentials: true // allows HTTP-only cookies to be sent
 });
 
-export const createItinerary = async (itineraryData) => {
-    return await API.post('/customer/create-itinerary', itineraryData);
-}
+// Itinerary API calls
+// export const createItinerary = async (itineraryData) => {
+//     return await API.post('/itineraries', itineraryData);
+// };
+
+// export const getCustomerItineraries = async () => {
+//     return await API.get('/itineraries');
+// };
+
+// export const getItineraryById = async (id) => {
+//     return await API.get(`/itineraries/${id}`);
+// };
+
+// export const updateItinerary = async (id, itineraryData) => {
+//     return await API.put(`/itineraries/${id}`, itineraryData);
+// };
+
+// export const deleteItinerary = async (id) => {
+//     return await API.delete(`/itineraries/${id}`);
+// };
+
+// // Add room to itinerary
+// export const addRoomToItinerary = async (itineraryId, roomData) => {
+//     return await API.post(`/itineraries/${itineraryId}/rooms`, roomData);
+// };
+
+// // Add schedule item to itinerary
+// export const addScheduleItem = async (itineraryId, scheduleData) => {
+//     return await API.post(`/itineraries/${itineraryId}/schedule`, scheduleData);
+// };
+
+// // Get all items for an itinerary
+// export const getItineraryItems = async (itineraryId) => {
+//     return await API.get(`/itineraries/${itineraryId}/items`);
+// };

2a84ffd3539dc678fc4afc0cff8fafe7e144278d
ipash185 <127605678+ipash185@users.noreply.github.com>
2025-05-01 11:56:55 +0530

    backend url hosting changes

--- src/api/customer.jsx
+++ src/api/customer.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://backend-hktx.onrender.com/api',
     withCredentials: true // allows HTTP-only cookies to be sent
 });
 
